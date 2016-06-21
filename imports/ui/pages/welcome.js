a1d55a2af2557c2755a22088b9ebc1c2ad3538ed
retotrinkler <rt@hedge-suisse.com>
2016-06-21 18:16:36 +0200

    .svg

--- imports/ui/pages/welcome.js
+++ imports/ui/pages/welcome.js
@@ -6,6 +6,7 @@ import './welcome.html';
 // Components used inside the template
 import '../components/email.js';
 
+
 Template.welcome.onRendered(function welcomeOnRendered() {
   this.$('.parallax').parallax();
 });
