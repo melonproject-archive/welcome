8a95ced75cfcf66f2c8a1ea933df1681c831720a
retotrinkler <reto.trinkler@gmail.com>
2016-08-02 17:53:20 +0200

    new welcome site

--- imports/ui/layouts/footer.js
+++ imports/ui/layouts/footer.js
@@ -1,3 +1,69 @@
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
 import { TAPi18n } from 'meteor/tap:i18n';
+import { $ } from 'meteor/jquery';
+import { MailChimp } from 'meteor/miro:mailchimp';
 
 import './footer.html';
+
+
+Template.layout_footer.onRendered(function layoutFooterOnRendered() {
+  this.$('.parallax').parallax();
+
+  const template = this;
+  this.$('#mailing-list').validate({
+    rules: {
+      emailAddress: {
+        email: true,
+        required: true,
+      },
+    },
+    messages: {
+      emailAddress: {
+        email: 'Please use a valid email address!',
+        required: 'An email address is required.',
+      },
+    },
+    errorPlacement(error) {
+      $('.error-message').text(error[0].innerText);
+    },
+    success(error) {
+      $('.error-message').text(error[0].innerText);
+    },
+    submitHandler() {
+      handleSubscriber({
+        email: template.find('[name="emailAddress"]').value,
+        action: 'subscribe',
+      });
+    },
+  });
+});
+
+Template.layout_footer.events({
+  'submit form': function(event) {
+    event.preventDefault();
+  },
+});
+
+// Methods used
+handleSubscriber = function(subscriber) {
+  Meteor.call( 'handleSubscriber', subscriber, function( error, response ) {
+    if (error) {
+      Materialize.toast(error.reason, 8000, 'orange') // 4000 is the duration of the toast
+    } else {
+      if ( response.complete || response.euid ) {
+        var subscribeMessage   = 'Please confirm your email to complete your subscription',
+            unsubscribeMessage = subscriber.email + ' successfully unsubscribed!',
+            message            = subscriber.action === 'subscribe' ? subscribeMessage : unsubscribeMessage;
+
+        Materialize.toast(message, 8000, 'green') // 4000 is the duration of the toast
+
+        if (template) {
+          template.getSubscribers();
+        }
+      } else {
+        Materialize.toast(response.message, 8000, 'orange') // 4000 is the duration of the toast
+      }
+    }
+  });
+};
