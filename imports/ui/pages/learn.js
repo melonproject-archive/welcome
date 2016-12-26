ce1ae91f4577d1aeb59f0b81d54a7a01beb55958
retotrinkler <reto.trinkler@gmail.com>
2016-12-26 15:41:25 +0100

    learn; about; contribution and new welcome site

--- imports/ui/pages/learn.js
+++ imports/ui/pages/learn.js
@@ -1,8 +1,4 @@
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
-import { FlowRouter } from 'meteor/kadira:flow-router';
 
 import './learn.html';
-
-Template.learn.onRendered(function learnOnRendered() {
-});
